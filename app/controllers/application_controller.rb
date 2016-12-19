0ad9d40eed6072264c42ef186764b04c1bdfab50
entcheva <christina.entcheva@gmail.com>
2016-12-19 13:35:59 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,33 +13,35 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    @post = Post.create(params[:name], params[:content])
-    erb :index
+    @post = Post.create(:name => params[:name], :content => params[:content])
+    redirect to '/posts/'
   end
 
-
-  # READ A POST
+  # READ ALL POSTS
   get '/posts' do
-    Post.all
+    @posts = Post.all
     erb :index
   end
 
-  # READ
+  # READ ONE POST
   get '/posts/:id' do
-    # display a specific post
+    @post = Post.find_by_id(params[:id])
     erb :show
-    # The get '/posts/:id' controller action handles requests for a given instance of your post. For example, if a user types in www.yourwebsite.com/posts/2, this route will catch that request and get the id number, in this case 2, from the params. It will then find the instance of the post with that id number and set it equal to an instance variable: @post = Model.find(params[:id]). Finally, it will render the show.erb view page.
   end
 
   # UPDATE
   get '/posts/:id/edit' do
-    # edit a specific post (take in the edit)
+    @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
   # UPDATE
   patch '/posts/:id' do
-    # execute the edit
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    redirect to "/posts/#{@post.id}"
   end
 
   # DELETE
